37c38cbe6108c9f9b7b925db8c993010d72f6257
zestones <idrissbenguezzou@gmail.com>
2024-05-18 23:02:53 +0200

    fix: change restarting time to 2hours

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -34,7 +34,7 @@ analyze_network_traffic() {
     fi
 
     current_time=$(date +%s)
-    restarting_time=30    # 2 hours
+    restarting_time=7200    # 2 hours
     last_date_time_seconds=$(date -d "$last_date_time" +%s)
 
     # Check if the last date and time is within the specified time difference
